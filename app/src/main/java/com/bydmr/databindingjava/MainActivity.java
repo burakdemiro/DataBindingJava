677dc7c39951ab5eca8548fadc8ce83d925d76a7
Burak Demir <demiryusufburak@gmail.com>
2019-02-21 11:00:04 +0300

    ilk databinding örneği

--- app/src/main/java/com/bydmr/databindingjava/MainActivity.java
+++ app/src/main/java/com/bydmr/databindingjava/MainActivity.java
@@ -1,14 +1,25 @@
 package com.bydmr.databindingjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.bydmr.databindingjava.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Pascal versiyon adlandırma
+    // activity_urunler -> ActivityUrunlerBinding üretilir
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Ogrenci ogr1 = new Ogrenci("Kamil", 5);
+
+        mainBinding.setOgrenci(ogr1);
     }
 }
